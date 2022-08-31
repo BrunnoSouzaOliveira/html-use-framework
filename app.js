b2a3058a92d0df7f0f56ea74ac091ba309490eaa
Brunno de Souza Oliveira <110069192+BrunnoSouzaOliveira@users.noreply.github.com>
2022-08-30 22:27:31 -0300

    Adicionando function:'post'

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-function requestOfServer(){
+function requestOfServerGet(){
     $.ajax({
         type: 'get',
-        url: "http://localhost:8000/hello-world?info=Good-Morning-Vietnam"
+        url: "http://localhost:8000/hello-world?info=Getting"
+    }).done(function(data){
+        document.getElementById('developer').innerHTML = data.name
+        document.getElementById('version').innerHTML = data.version
+        document.getElementById('value_of_variable').innerHTML = data.value_of_variable_info
+        document.getElementById('company_site').innerHTML = data.web_site_company
+
+        $("#show-values").css('display','block')
+    });
+}
+
+function requestOfServerPost(){
+    $.ajax({
+        type: 'post',
+        url: "http://localhost:8000/hello-rota?info=Posting"
     }).done(function(data){
         document.getElementById('developer').innerHTML = data.name
         document.getElementById('version').innerHTML = data.version
